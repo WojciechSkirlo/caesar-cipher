66fbfb93be5b474d48c236a484dfde3a76c141cd
Wojciech Skirło <wojciechskiro@gmail.com>
2022-10-17 20:05:42 +0200

    add PWA app

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,35 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Caesar cipher",
+        short_name: "CaCipher",
+        description: "Caesar cipher - encrypt and decrypt your text",
+        theme_color: "#f3f4f6",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
